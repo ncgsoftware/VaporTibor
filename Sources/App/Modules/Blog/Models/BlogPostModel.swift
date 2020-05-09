9552f3d9aa9a170852e0de130373faaef07fd32e
Jeremy Nelson <ncgsoftware@gmail.com>
2020-05-09 15:59:13 -0400

    Chapter 4 - Finished

--- Sources/App/Modules/Blog/Models/BlogPostModel.swift
+++ Sources/App/Modules/Blog/Models/BlogPostModel.swift
@@ -4,7 +4,7 @@ import Fluent
 final class BlogPostModel: Model {
     
     static let schema: String = "blog_posts"
-    
+
     struct FieldKeys {
         static var title: FieldKey { "title" }
         static var slug: FieldKey { "slug" }
@@ -15,24 +15,26 @@ final class BlogPostModel: Model {
         static var categoryId: FieldKey { "categoryId" }
     }
     
-    
     @ID() var id: UUID?
-    @Field(key: FieldKeys.title ) var title: String
-    @Field(key: FieldKeys.slug ) var slug: String
-    @Field(key: FieldKeys.image ) var image: String
-    @Field(key: FieldKeys.excerpt ) var excerpt: String
-    @Field(key: FieldKeys.date ) var date: Date
-    @Field(key: FieldKeys.content ) var content: String
-    @Parent(key: FieldKeys.categoryId ) var category: BlogCategoryModel
+    @Field(key: FieldKeys.title) var title: String
+    @Field(key: FieldKeys.slug) var slug: String
+    @Field(key: FieldKeys.image) var image: String
+    @Field(key: FieldKeys.excerpt) var excerpt: String
+    @Field(key: FieldKeys.date) var date: Date
+    @Field(key: FieldKeys.content) var content: String
+    @Parent(key: FieldKeys.categoryId) var category: BlogCategoryModel
 
-    public init(id: UUID? = nil,
-                title: String,
-                slug: String,
-                image: String,
-                excerpt: String,
-                date: Date,
-                content: String,
-                categoryId: UUID) {
+    init() { }
+
+    init(id: UUID? = nil,
+         title: String,
+         slug: String,
+         image: String,
+         excerpt: String,
+         date: Date,
+         content: String,
+         categoryId: UUID)
+    {
         self.id = id
         self.title = title
         self.slug = slug
@@ -42,8 +44,29 @@ final class BlogPostModel: Model {
         self.content = content
         self.$category.id = categoryId
     }
-    
-    init() {}
-    
-    
+}
+
+extension BlogPostModel {
+
+    struct ViewContext: Encodable {
+        var id: String
+        var title: String
+        var slug: String
+        var image: String
+        var excerpt: String
+        var date: String
+        var content: String
+
+        init(model: BlogPostModel) {
+            self.id = model.id!.uuidString
+            self.title = model.title
+            self.slug = model.slug
+            self.image = model.image
+            self.excerpt = model.excerpt
+            self.date = DateFormatter.year.string(from: model.date)
+            self.content = model.content
+        }
+    }
+
+    var viewContext: ViewContext { .init(model: self) }
 }
